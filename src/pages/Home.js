73d1dd9a0ee16ae293334d7fed0044b3887ba68c
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-08-09 13:25:04 -0700

    Complete home page and add photos

--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-class Home {
-  render = () => {
-    const container = document.createElement('div');
-    container.className = 'home-container';
-    return container;
-  };
-}
-
-export default Home;
