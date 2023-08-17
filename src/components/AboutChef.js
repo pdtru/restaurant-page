88bad514c18fe4c287d7eb08cf190dabe2c92794
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-08-17 15:42:52 -0700

    Complete about page

--- src/components/AboutChef.js
+++ src/components/AboutChef.js
@@ -1,29 +1,52 @@
 class AboutChef {
+  cotainerClassName;
+  headerClassName;
+  subHeaderClassName;
+  textClassName;
+  sanjiClassName;
+
+  calculateLayout = () => {
+    if (window.innerWidth < window.innerHeight) {
+      this.cotainerClassName = 'mobile-about-chef-container';
+      this.headerClassName = 'mobile-about-chef-header';
+      this.subHeaderClassName = 'mobile-about-chef-sub-header';
+      this.textClassName = 'mobile-about-chef-text';
+      this.sanjiClassName = 'mobile-sanji';
+    } else {
+      this.cotainerClassName = 'about-chef-container';
+      this.headerClassName = 'about-chef-header';
+      this.subHeaderClassName = 'about-chef-sub-header';
+      this.textClassName = 'about-chef-text';
+      this.sanjiClassName = 'sanji';
+    }
+  };
+
   render = () => {
+    this.calculateLayout();
     const container = document.createElement('div');
-    container.className = 'about-chef-container';
+    container.className = this.cotainerClassName;
 
     const header = document.createElement('p');
-    header.className = 'about-chef-header';
+    header.className = this.headerClassName;
     header.innerText = 'Meet Our';
 
     const subHeader = document.createElement('p');
-    subHeader.className = 'about-chef-sub-header';
+    subHeader.className = this.subHeaderClassName;
     subHeader.innerText = 'CHEF';
 
     const chefContainer = document.createElement('div');
     chefContainer.className = 'about-chef';
 
     const text = document.createElement('p');
-    text.className = 'about-chef-text';
+    text.className = this.textClassName;
     text.innerText =
       'At the heart of our kitchen stands the indomitable culinary genius, Owner and Head Chef Sanji. Sanji brings to life a menu that mirrors his adventurous spirit - each dish a masterpiece, an embodiment of his passion and mastery over both Japanese and French cuisine.';
 
     const sanji = document.createElement('img');
-    sanji.className = 'sanji';
+    sanji.className = this.sanjiClassName;
     sanji.src = '/src/assets/images/sanji.jpg';
 
-    chefContainer.append(text, sanji);
+    chefContainer.append(sanji, text);
     container.append(header, subHeader, chefContainer);
     return container;
   };
--- src/components/AboutStory.js
+++ src/components/AboutStory.js
@@ -1,18 +1,38 @@
 class AboutStory {
+  containerClassName;
+  logoClassName;
+  headerClassName;
+  textClassName;
+
+  calculateLayout = () => {
+    if (window.innerWidth < window.innerHeight) {
+      this.containerClassName = 'mobile-about-story-container';
+      this.logoClassName = 'mobile-about-story-logo';
+      this.headerClassName = 'mobile-about-story-header';
+      this.textClassName = 'mobile-about-story-text';
+    } else {
+      this.containerClassName = 'about-story-container';
+      this.logoClassName = 'about-story-logo';
+      this.headerClassName = 'about-story-header';
+      this.textClassName = 'about-story-text';
+    }
+  };
+
   render = () => {
+    this.calculateLayout();
     const container = document.createElement('div');
-    container.className = 'about-story-container';
+    container.className = this.containerClassName;
 
     const logo = document.createElement('p');
-    logo.className = 'about-story-logo';
+    logo.className = this.logoClassName;
     logo.innerText = 'Tout Bleu';
 
     const header = document.createElement('p');
-    header.className = 'about-story-header';
-    header.innerText = 'OUR STORY';
+    header.className = this.headerClassName;
+    header.innerText = 'STORY';
 
     const text = document.createElement('p');
-    text.className = 'about-story-text';
+    text.className = this.textClassName;
     text.innerText =
       "Welcome to Tout Bleu where you'll immerse yourself in the harmonious elegance of the French and Japanese culinary arts. At Tout Bleu, we invite you on a gastronomic journey that transcends borders and indulges your senses in the harmonious blend of French and Japanese culinary traditions. Nestled at the heart of the Grand Line, our restaurant is a serene oasis where innovation meets authenticity, and where every dish tells a story of cultural fusion.";
 
